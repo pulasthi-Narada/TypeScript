0496fabca0bfe0013c7da95fd62ad5a2a59b091c
Pulasthi Narada <pulasthinarada@hotmail.com>
2022-08-02 18:48:48 +0530

    index files

---
+++ index.js
@@ -0,0 +1,32 @@
+var movies = 'groudhog day';
+movies.toString();
+//hhh
+var g = 'true';
+var num = 12;
+num = null;
+var r = 4;
+r();
+r = false;
+var n;
+n = 2;
+n = true;
+var nn;
+nn = 1;
+nn = false;
+var movies = ['now you see me', 'now you see me 2'];
+var foundMovie;
+for (var _i = 0, movies_1 = movies; _i < movies_1.length; _i++) {
+    var m = movies_1[_i];
+    m === 'now you see me 2' ? (foundMovie = m) : (foundMovie = '');
+}
+function numPlus(h) {
+    return h + h;
+}
+var numPlus2 = function (n) { return n + n; };
+var numPlus3 = function (n) {
+    if (n === void 0) { n = 1; }
+    return n + n;
+};
+numPlus(1);
+numPlus('sss');
+numPlus3(1);
---
+++ index.ts
@@ -0,0 +1,32 @@
+var movies: string = 'groudhog day';
+
+movies.toString();
+//hhh
+var g: boolean = 'true';
+
+let num = 12;
+
+num = null;
+
+let r: any = 4;
+r();
+r = false;
+
+let n;
+
+n = 2;
+n = true;
+
+let nn: number;
+
+nn = 1;
+
+nn = false;
+
+const movies = ['now you see me', 'now you see me 2'];
+
+let foundMovie;
+
+for (let m of movies) {
+  m === 'now you see me 2' ? (foundMovie = m) : (foundMovie = '');
+}
